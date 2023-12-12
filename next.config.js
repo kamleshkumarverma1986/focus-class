c0e06c33e1719b825b152f9a2a758bd90b5f12d7
Kamlesh Kumar Verma <kkumarverma@MXLKH21QPD.local>
2023-12-12 23:35:05 +0530

    done

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ["www.focusclass.in/"],
+    },
+}
 
 module.exports = nextConfig
